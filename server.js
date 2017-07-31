50838149fe6551893d144d548281e1102899d2af
Aizaz <bitf14m024@pucit.edu.pk>
2017-07-31 19:31:10 +0500

    updates

--- server.js
+++ server.js
@@ -17,6 +17,3 @@ const path = require('path');
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
