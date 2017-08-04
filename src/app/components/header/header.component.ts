a33b99b2e6be4c6948c5d9e1d923f76d27c10695
Aizaz <bitf14m024@pucit.edu.pk>
2017-08-04 23:38:04 +0500

    categories

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../../services/data.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,50 @@ import { Component, OnInit } from '@angular/core';
   
 })
 export class HeaderComponent implements OnInit {
+userData: any;  
+subcat:any;
+keysub:string[];
 
-  constructor() { }
+ keys: String[];
+  constructor(private dataservice:DataService,private router: Router) { 
+  		this.getCategory();
+  }
 
   ngOnInit() {
+  }
+  getCategory(){
+  	this.dataservice.getCategories().subscribe(
+  		result=>{
+  			console.log("get categories");
+  				if(result.status==true){
+  					this.userData=result.categories;
+  					this.keys=Object.keys(this.userData	);
+  					console.log(this.userData);
+  				}
+  				else{
+  					console.log("false");
+  				}
+
+
+  		});
+
+  }
+
+  sub(data){
+  	console.log("in sub");
+  	console.log(data);
+  	this.dataservice.getSubCategories(data).subscribe(
+  		result=>{
+  			console.log("sub cate");
+  			if(result.status==true){
+  				console.log(result.subcategories)
+  				this.subcat=result.subcategories;
+  				this.keysub=Object.keys(this.subcat);
+
+  			}
+  		
+
+  	});
   }
 
 }
